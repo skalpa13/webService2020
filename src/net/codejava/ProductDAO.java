71c507aa479eebe1c4281de8056d679011aadd57
1800146 <1800146@POR7203334.exchange.ad.afpanet>
2020-09-09 12:02:06 +0200

    static id

--- src/net/codejava/ProductDAO.java
+++ src/net/codejava/ProductDAO.java
@@ -5,15 +5,17 @@ import java.util.List;
 
 public class ProductDAO {
 	private static ProductDAO instance;
+	private static int id=3;
+	
 	private static List<Product> data = new ArrayList<>();
 	
-	
-	
-	
-	
-	public ProductDAO() {
+	static {
 		data.add(new Product(1, "iPhone X", 999.99f));
 		data.add(new Product(2, "XBOX 360", 329.50f));
+	}
+	
+	private ProductDAO() {
+		
 	}
 	
 	public static ProductDAO getInstance() {
@@ -29,11 +31,11 @@ public class ProductDAO {
 	}
 	
 	public int add(Product product) {
-		int newId = data.size() + 1;
-		product.setId(newId);
+		//int newId = data.size() + 1;
+		product.setId(id);
+		id++;
 		data.add(product);
-		
-		return newId;
+		return id;
 	}
 	
 	public Product get(int id) {
